0d54657b926496be7175062691b2d35627e7200c
JohnMark13 <mark.findlater@googlemail.com>
2023-03-31 18:03:53 +0100

    OK, quite a hacky way to match, but, matches

--- scripts/update-ledger-from-chain.js
+++ scripts/update-ledger-from-chain.js
@@ -19,7 +19,7 @@ async function updateLedgerFromChain() {
     console.info('updateLedgerFromChain');
 
     //Get access to the ledger and create a writable version
-    const ledgerManager = new sc.ledger.manager.LedgerManager({storage: new sc.ledger.storage.WritableGithubStorage({apiToken: process.env.SOURCECRED_GITHUB_TOKEN, repo: config.repo, branch: config.branch})})
+    const ledgerManager = new sc.ledger.manager.LedgerManager({storage: new sc.ledger.storage.WritableGithubStorage({apiToken: process.env.SOURCECRED_GITHUB_TOKEN, repo: config.repo, branch: config.branch, message:"Testing message"})})
     await ledgerManager.reloadLedger()
     console.info(`Ledger instance created`);
                                      
@@ -35,70 +35,90 @@ async function updateLedgerFromChain() {
 
     console.info(`Read Citizen Disource data from Github`);
 
-    processGitHubCitizens(ledgerManager, githubFile);
+    const lowerAccountToIdentityMap = new Map();
 
+    ledgerManager.ledger._aliasAddressToIdentity.forEach((value, key) => {
+        lowerAccountToIdentityMap.set(key.toLowerCase(),value);
+    });
+
+    await processGitHubCitizens(ledgerManager, lowerAccountToIdentityMap, githubFile);
 }
 
-async function processGitHubCitizens(ledgerManager, githubFile) {
-    const readable = Readable.from(githubFile);
-    readable.pipe(csvParser())
-        .on('data', async (row) => {
-            console.info(`Reading GitHub data for: ${row.passport_id}`);
-            const ghUsername = row.github_username;
-            const ghUsernameENS = row.github_username_ens;
+async function processGitHubCitizens(ledgerManager, lowerAccountToIdentityMap, githubFile) {
+    const promise = new Promise(function(resolve, reject)  {
+        const readable = Readable.from(githubFile);
+        readable.pipe(csvParser())
+            .on('data', (row) => {
+                console.info(`Reading GitHub data for: ${row.passport_id}`);
+                const ghUsername = row.github_username;
+                const ghUsernameENS = row.github_username_ens;
 
-            if ((!ghUsername) && (!ghUsernameENS)) {
-                console.info(`No GitHub username for passport ${row.passport_id}`)
-                return;
-            }
-
-            //get just the username from one of the two columns
-            const githubUsername = ghUsername && ghUsername.length > 0
-             ? cleanGithubUsername(ghUsername)
-             : cleanGithubUsername(ghUsernameENS);
-
-            //construct identity
-            const ghAddress = sc.core.graph.NodeAddress.fromParts([
-                "sourcecred",
-                "github",
-                "USERLIKE",
-                "USER",
-                githubUsername
-            ]);
-
-            //{"action":{"identity":{"address":"N\u0000sourcecred\u0000core\u0000IDENTITY\u0000VKoeRG5eG3wFrPwUP30quA\u0000","aliases":[],"id":"VKoeRG5eG3wFrPwUP30quA","name":"johnmark13-github","subtype":"USER"},"type":"CREATE_IDENTITY"},"ledgerTimestamp":1652751063468,"uuid":"l011nxUv5Cy7bsGjhgpZqw","version":"1"}
-            //{"action":{"alias":{"address":"N\u0000sourcecred\u0000github\u0000USERLIKE\u0000USER\u0000johnmark13\u0000","description":"github/[@johnmark13](https://github.com/johnmark13)"},"identityId":"VKoeRG5eG3wFrPwUP30quA","type":"ADD_ALIAS"},"ledgerTimestamp":1652751063469,"uuid":"i57Hyx47nL5OUy01sxFqhA","version":"1"}
-            //'NsourcecredgithubUSERLIKEuserJohnMark13'
-            const account = ledgerManager.ledger.accountByAddress(ghAddress);
-            if(account) {
-                //ah it already exists, check payout address matches
-                const uuid = account.identity.id;
-                const existingAddress = account.payoutAddresses.get(`{"chainId":"${config.chainId}","tokenAddress":"${config.tokenAddress}","type":"EVM"}`);
-
-                if(!existingAddress) {
-                    console.log(`There was not a payout address set for passport ${row.passport_id} - gitHubUsername ${githubUsername}`);
+                if ((!ghUsername) && (!ghUsernameENS)) {
+                    console.info(`No GitHub username for passport ${row.passport_id}`)
+                    return;
                 }
-                
-                if(existingAddress && existingAddress.toLowerCase() === row.owner_address.toLowerCase()) {
-                    console.log(`Correct payout address already set for passport ${row.passport_id} - gitHubUsername ${githubUsername}`)
+
+                //get just the username from one of the two columns
+                const githubUsername = ghUsername && ghUsername.length > 0
+                ? cleanGithubUsername(ghUsername)
+                : cleanGithubUsername(ghUsernameENS);
+
+                //construct identity
+                const ghAddress = sc.core.graph.NodeAddress.fromParts([
+                    "sourcecred",
+                    "github",
+                    "USERLIKE",
+                    "USER",
+                    githubUsername
+                ]);
+
+                //{"action":{"identity":{"address":"N\u0000sourcecred\u0000core\u0000IDENTITY\u0000VKoeRG5eG3wFrPwUP30quA\u0000","aliases":[],"id":"VKoeRG5eG3wFrPwUP30quA","name":"johnmark13-github","subtype":"USER"},"type":"CREATE_IDENTITY"},"ledgerTimestamp":1652751063468,"uuid":"l011nxUv5Cy7bsGjhgpZqw","version":"1"}
+                //{"action":{"identity":{"address":"N\u0000sourcecred\u0000core\u0000IDENTITY\u0000HLMS4JjpNt38zUJpLmteWg\u0000","aliases":[],"id":"HLMS4JjpNt38zUJpLmteWg","name":"xPi2","subtype":"USER"},"type":"CREATE_IDENTITY"},"ledgerTimestamp":1652442131578,"uuid":"vHbyyH7rKyiveNpjGMPqpQ","version":"1"}
+                //{"action":{"alias":{"address":"N\u0000sourcecred\u0000github\u0000USERLIKE\u0000USER\u0000johnmark13\u0000","description":"github/[@johnmark13](https://github.com/johnmark13)"},"identityId":"VKoeRG5eG3wFrPwUP30quA","type":"ADD_ALIAS"},"ledgerTimestamp":1652751063469,"uuid":"i57Hyx47nL5OUy01sxFqhA","version":"1"}
+                //{"action":{"alias":{"address":"N\u0000sourcecred\u0000github\u0000USERLIKE\u0000USER\u0000xPi2\u0000","description":"github/[@xPi2](https://github.com/xPi2)"},"identityId":"HLMS4JjpNt38zUJpLmteWg","type":"ADD_ALIAS"},"ledgerTimestamp":1652442131578,"uuid":"ZuxcEcrMrDX6YoY0qIJPMg","version":"1"}
+
+                const hasAccount = lowerAccountToIdentityMap.has(ghAddress.toLowerCase());
+                if(hasAccount) {
+                    const uuid = lowerAccountToIdentityMap.get(ghAddress.toLowerCase());
+                    const account = ledgerManager.ledger.account(uuid);
+                    //ah it already exists, check payout address matches
+                    
+                    const existingAddress = account.payoutAddresses.get(`{"chainId":"${config.chainId}","tokenAddress":"${config.tokenAddress}","type":"EVM"}`);
+
+                    if(!existingAddress) {
+                        console.log(`There was not a payout address set for passport ${row.passport_id} - gitHubUsername ${githubUsername}`);
+                    }
+                    
+                    if(existingAddress && existingAddress.toLowerCase() === row.owner_address.toLowerCase()) {
+                        console.log(`Correct payout address already set for passport ${row.passport_id} - gitHubUsername ${githubUsername}`)
+                    }
+                    else {
+                        console.log(`Payout address not correctly set for passport ${row.passport_id} - gitHubUsername ${githubUsername}`)
+                        
+                        ledgerManager.ledger.setPayoutAddress(uuid, row.owner_address , config.chainId, config.tokenAddress);
+
+                        console.log(`Updated payout address to ${row.owner_address} for passport ${row.passport_id} - gitHubUsername ${githubUsername}`)
+                    }                
                 }
                 else {
-                    //ledgerManager.ledger.setPayoutAddress(uuid, row.owner_address , config.chainId, config.tokenAddress);
-                }                
-            }
-            else {
-                //create identity
+                    //create identity
 
-                //setPayoutAddress for new identity
-            }
-        });
+                    //setPayoutAddress for new identity
+                }
+            })
+            .on('end', () => {
+                console.log('GitHub CSV file successfully processed');
+                resolve(true);
+            })
+    });
 
-        const result = await ledgerManager.persist();
+    await promise;
 
-        if (result.error) {
-            console.error(`Filed to update the ledger: ${result.error}`);
-            throw result.error
-        };
+    const result = await ledgerManager.persist();
+    if (result.error) {
+        console.error(`Failed to update the ledger: ${result.error}`);
+        throw result.error
+    };
 }
 
 function cleanGithubUsername(ghUsername) {
